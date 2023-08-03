ebf9389d78c96b65fc98d67d8f2fdf1707fe1e37
cheif22 <37961102+cheif22@users.noreply.github.com>
2023-08-03 18:25:41 -0400

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -137,5 +137,15 @@ class CfgMusic
 //Script fire offs
 class Extended_PostInit_EventHandlers
 {
- MC_Adapt_Post_Init = "A3_Adapt_Post_Init_Var = [] execVM ""A3_Adapt\init.sqf""";
+	class A3_adapt
+	{
+		clientInit="[] execVM ""A3_adapt\init.sqf""";
+	};
+};
+class Extended_PreInit_EventHandlers
+{
+	class A3_adapt
+	{
+		clientInit="call compilescript [""A3_adapt\CBAsetting.sqf""]";
+	};
 };
